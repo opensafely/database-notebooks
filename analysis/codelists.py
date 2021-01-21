f5fa74037bf88b16bb2b4f039054e5430929854e
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-01-21 10:31:04 +0000

    add study definition + codelists

---
+++ analysis/codelists.py
@@ -0,0 +1,11 @@
+from cohortextractor import (
+    codelist,
+    codelist_from_csv,
+)
+
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
